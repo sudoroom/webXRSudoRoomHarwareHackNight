a0f6a409f541cbc91b3641d63df129cbd0994f6a
romy <romy@snowyla.com>
2024-08-13 17:13:56 -0700

    less gross directory structure

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// You can add any additional JavaScript here
+// For example, you might want to add event listeners or other functionality
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('VR Scene loaded');
+});
---
+++ js/pinch-handler.js
@@ -0,0 +1,30 @@
+AFRAME.registerComponent('pinch-handler', {
+    init: function() {
+        this.skyboxEl = document.querySelector('#skybox');
+        this.skyboxTextures = ['#skyTexture1', '#skyTexture2'];
+        this.currentTextureIndex = 0;
+        
+        this.el.sceneEl.addEventListener('enter-vr', () => {
+            const session = this.el.sceneEl.xrSession;
+            if (session) {
+                this.setupEventListeners(session);
+            }
+        });
+    },
+    
+    setupEventListeners: function(session) {
+        session.addEventListener('select', this.onSelect.bind(this));
+    },
+    
+    onSelect: function(event) {
+        if (event.inputSource.targetRayMode === 'transient-pointer') {
+            this.changeSkybox();
+        }
+    },
+    
+    changeSkybox: function() {
+        this.currentTextureIndex = (this.currentTextureIndex + 1) % this.skyboxTextures.length;
+        const newTexture = this.skyboxTextures[this.currentTextureIndex];
+        this.skyboxEl.setAttribute('src', newTexture);
+    }
+});

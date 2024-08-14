d0b67c2a21aeeaecbf6beecfc969f6c42ef4c90a
romy <romy@snowyla.com>
2024-08-13 21:30:21 -0700

    doesn't work

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-// You can add any additional JavaScript here
-// For example, you might want to add event listeners or other functionality
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('VR Scene loaded');
-});
+    const skybox = document.querySelector('#skybox');
+    const portal1 = document.querySelector('#portal1');
+    const portal2 = document.querySelector('#portal2');
+    const cursor = document.querySelector('[cursor]');
 
-const video = document.querySelector('#360-video');
-const playButton = document.querySelector('#playButton');
+    const skyboxTextures = ['#skyTexture1', '#skyTexture2', '#skyTexture3'];
+    let currentTextureIndex = 0;
 
-playButton.addEventListener('click', function() {
-  video.play();
-  playButton.style.display = 'none';
+    function changeScene(portalElement) {
+        currentTextureIndex = (currentTextureIndex + 1) % skyboxTextures.length;
+        skybox.setAttribute('src', skyboxTextures[currentTextureIndex]);
+        
+        // Toggle visibility of portals
+        portal1.setAttribute('visible', !portal1.getAttribute('visible'));
+        portal2.setAttribute('visible', !portal2.getAttribute('visible'));
+    }
+
+    function handleGaze(event) {
+        const gazedElement = event.detail.intersectedEl;
+        if (gazedElement.classList.contains('portal')) {
+            changeScene(gazedElement);
+        }
+    }
+
+    cursor.addEventListener('fusing', handleGaze);
 });
